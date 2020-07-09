7a2f8942c36d66293645243c82d2e78577c807ab
Ivan Yakovlev <ivanvyakovlev@gmail.com>
2020-07-10 00:59:36 +0300

    feat(errors): add custom errors classes and errorHandler middleware

---
+++ auth/src/errors/DatabaseConnectionError.ts
@@ -0,0 +1,14 @@
+import { CustomError } from './CustomError';
+
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
+  reason = 'Failed connect to database';
+  constructor() {
+    super('Failed connect to database');
+
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+  serializeErrors() {
+    return [{ message: this.reason }];
+  }
+}
