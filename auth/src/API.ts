b5209a1055698a651e35f4d61dec6ff7501c38cf
Ivan Yakovlev <ivanvyakovlev@gmail.com>
2020-07-10 01:01:37 +0300

    feat(api): add API constants

---
+++ auth/src/API.ts
@@ -0,0 +1,8 @@
+export const baseUrl = '/api/users';
+
+export const UserAPI = {
+  currentUser: baseUrl + '/currentuser',
+  signUp: baseUrl + '/signup',
+  signIn: baseUrl + '/signin',
+  signOut: baseUrl + '/signout',
+};
