e346b518e8ccea7e37a139099a26b824ef6c13ac
Ivan Yakovlev <ivanvyakovlev@gmail.com>
2020-07-21 23:08:31 +0300

    refactor(auth): extract app to separate file

---
+++ auth/src/app.ts
@@ -0,0 +1,35 @@
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session'
+
+import { errorHandler } from './middlewares/errorHandler'
+import { NotFoundError } from './errors/NotFoundError'
+import { currentUserRouter } from './routes/currentUser'
+import { signInRouter } from './routes/signin'
+import { signOutRouter } from './routes/signout'
+import { signUpRouter } from './routes/signup'
+
+const app = express()
+app.set('trust proxy', 1)
+
+app.use(json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+)
+
+app.use(currentUserRouter)
+app.use(signInRouter)
+app.use(signOutRouter)
+app.use(signUpRouter)
+
+app.get('*', () => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,5 @@
-import express from 'express'
-import 'express-async-errors'
-import { json } from 'body-parser'
 import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
-
-import { errorHandler } from './middlewares/errorHandler'
-import { NotFoundError } from './errors/NotFoundError'
-import { currentUserRouter } from './routes/currentUser'
-import { signInRouter } from './routes/signin'
-import { signOutRouter } from './routes/signout'
-import { signUpRouter } from './routes/signup'
-
-const app = express()
-app.set('trust proxy', 1)
-
-app.use(json())
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-)
-
-app.use(currentUserRouter)
-app.use(signInRouter)
-app.use(signOutRouter)
-app.use(signUpRouter)
-
-app.get('*', () => {
-  throw new NotFoundError()
-})
-
-app.use(errorHandler)
+import { app } from './app'
 
 async function startDb() {
   if (!process.env.JWT_KEY) {
