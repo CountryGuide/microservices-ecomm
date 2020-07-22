f632c2e2ecd1acf5dbe2b347e9e710d051dae758
Ivan Yakovlev <ivanvyakovlev@gmail.com>
2020-07-22 21:39:38 +0300

    refactor(auth): replace hardcoded mongo uri with env var

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,8 +5,11 @@ async function startDb() {
   if (!process.env.JWT_KEY) {
     throw new Error('No JWT key defined')
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('No MONGO_URI key defined')
+  }
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
