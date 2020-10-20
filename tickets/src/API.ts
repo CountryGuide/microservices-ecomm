406abeadd05b0b85f1002aea426882a999ddbe78
VanyaY <ivanvyakovlev@gmail.com>
2020-10-20 20:20:09 +0300

    refactor(api): refactor TicketsAPI

--- tickets/src/API.ts
+++ tickets/src/API.ts
@@ -1,22 +1,20 @@
-export const baseUrl = '/api/tickets'
-
 export const TicketsAPI = {
-  create: baseUrl,
-  read: baseUrl,
-  update: url`${baseUrl}/${'id'}`,
-  delete: url`${baseUrl}/${'id'}`,
+  index: url`/api/tickets`,
+  withId: url`/api/tickets/${'id'}`,
 }
 
 type Dict = { [key: string]: string | number }
 
 function url(strings: TemplateStringsArray, ...keys: (string | number)[]) {
-  return function (...values: []) {
+  return function (...values: any) {
     const dict: Dict = values[values.length - 1] || {}
     const result = [strings[0]]
+
     keys.forEach(function (key: string | number, i) {
       const value = Number.isInteger(key) ? values[key as number] : dict[key]
       result.push(value as string, strings[i + 1])
     })
+
     return result.join('')
   }
 }
