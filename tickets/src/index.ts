313d60d79507b8247702e044482d4209eea59503
VanyaY <ivanvyakovlev@gmail.com>
2020-11-15 22:23:45 +0300

    feat(nats): add nats wrapper

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import { app } from './app'
+import { natsWrapper } from './NatsWrapper'
 
 async function startDb() {
   if (!process.env.JWT_KEY) {
@@ -8,7 +9,26 @@ async function startDb() {
   if (!process.env.MONGO_URI) {
     throw new Error('No MONGO_URI key defined')
   }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('No NATS_CLIENT_ID key defined')
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('No NATS_CLUSTER_ID key defined')
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('No NATS_URL key defined')
+  }
   try {
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL)
+
+    natsWrapper.stan.on('close', () => {
+      console.log('NATS connection closed')
+      process.exit()
+    })
+
+    process.on('SIGINT', () => natsWrapper.stan.close())
+    process.on('SIGTERM', () => natsWrapper.stan.close())
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
