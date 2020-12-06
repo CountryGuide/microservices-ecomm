cf537927d9a0170c891bd449046c747d6b52db0b
VanyaY <ivanvyakovlev@gmail.com>
2020-12-07 00:17:46 +0300

    feat(payments): setup Payments service

---
+++ payments/src/index.ts
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+import { app } from './app'
+import { natsWrapper } from './NatsWrapper'
+
+async function startDb() {
+  if (!process.env.JWT_KEY) {
+    throw new Error('No JWT key defined')
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('No MONGO_URI key defined')
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('No NATS_CLIENT_ID key defined')
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('No NATS_CLUSTER_ID key defined')
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('No NATS_URL key defined')
+  }
+  try {
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL)
+
+    natsWrapper.stan.on('close', () => {
+      console.log('NATS connection closed')
+      process.exit()
+    })
+
+    process.on('SIGINT', () => natsWrapper.stan.close())
+    process.on('SIGTERM', () => natsWrapper.stan.close())
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+  } catch (e) {
+    console.error(e)
+  }
+
+  app.listen(3000)
+}
+
+startDb()
