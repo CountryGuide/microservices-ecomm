598f219da1eaaf6efecd6180432ac5de5c5fa784
VanyaY <ivanvyakovlev@gmail.com>
2020-12-07 00:55:18 +0300

    feat(payments): add listeners

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 import { app } from './app'
 import { natsWrapper } from './NatsWrapper'
+import { OrderCancelledListener } from './events/listeners/OrderCancelledListener'
+import { OrderCreatedListener } from './events/listeners/OrderCreatedListener'
 
 async function startDb() {
   if (!process.env.JWT_KEY) {
@@ -28,6 +30,9 @@ async function startDb() {
 
     process.on('SIGINT', () => natsWrapper.stan.close())
     process.on('SIGTERM', () => natsWrapper.stan.close())
+
+    new OrderCancelledListener(natsWrapper.stan).listen()
+    new OrderCreatedListener(natsWrapper.stan).listen()
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
